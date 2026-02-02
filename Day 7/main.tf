5cb9b2baab3e7b5d9c64e598f52f6dd02569d99f
Ali Sameed <alisameed.cs@gmail.com>
2026-02-02 20:44:11 +0500

    Day 7: Terraform Modules (Basics)

---
+++ Day 7/main.tf
@@ -0,0 +1,10 @@
+module "web_server" {
+  source         = "./modules/ec2-instance"
+  ami_id         = "ami-0c55b159cbfafe1f0"
+  instance_type  = "t2.micro"
+  instance_name  = "MyWebServer"
+}
+
+output "web_server_id" {
+  value = module.web_server.instance_id
+}
