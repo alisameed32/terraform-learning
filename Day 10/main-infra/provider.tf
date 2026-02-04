b3e3f5c0f91a19e44358c7662e1f9aa16f32c7d6
Ali Sameed <alisameed.cs@gmail.com>
2026-02-05 00:25:05 +0500

    Add backend setup for Terraform with S3 and DynamoDB

---
+++ Day 10/main-infra/provider.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+# AWS
+  backend "s3" {
+    bucket         = "my-terraform-remote-state-demo-bucket"  # from backend-setup output
+    key            = "demo/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-demo"
+    encrypt        = true
+  }
+}
+
+# Azure
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "terraform-rg"
+#     storage_account_name = "terraformstorageacct"
+#     container_name       = "tfstate"
+#     key                  = "prod.terraform.tfstate"
+#   }
+# }
+
+# GCP
+
+# terraform {
+#   backend "gcs" {
+#     bucket  = "terraform-state-demo"
+#     prefix  = "terraform/state"
+#   }
+
+#   backend "gcs" {
+#     bucket  = "terraform-state-demo"
+#     prefix  = "terraform/state"
+#   }
+# }
+
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ Day 10/main-infra/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name" {
+  type        = string
+  default     = "terraform-demo"
+  description = "Project name prefix for resources."
+}
